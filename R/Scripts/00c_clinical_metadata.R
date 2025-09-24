de127ba4fb58224a80df4d042d695a968ce9d189
jdpreston30 <jdpreston30@gmail.com>
2025-09-24 14:53:36 -0400

    comments

--- R/Scripts/00c_clinical_metadata.R
+++ R/Scripts/00c_clinical_metadata.R
@@ -55,7 +55,10 @@
       donor_age_col = "donor_age",
       ischemic_time_col = "operative_IT_minutes"
     ) %>%
-    select(Patient, preop_RADIAL_calc)
+    select(Patient, preop_RADIAL_calc) %>%
+    calc_ISHLT()
+    #!!!! postop_PGD_ISHLT (Y N)
+    #!!! postop_PGD_textbook_calc
 # #+ 0d.4: Break into components for the final tables
   T1_data <- clinical_metadata_i %>%
     select(Patient, postop_PGD_ISHLT, all_of(T1))
