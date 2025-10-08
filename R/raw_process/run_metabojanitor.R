641d17943fc42e8e961ecc6bb92947e6080e41fa
jdpreston30 <jdpreston30@gmail.com>
2025-10-08 10:09:55 -0400

    added option for combat yaml

--- R/raw_process/run_metabojanitor.R
+++ R/raw_process/run_metabojanitor.R
@@ -8,19 +8,27 @@ output_dir <- "/Users/JoshsMacbook2015/Library/CloudStorage/OneDrive-EmoryUniver
 raw_path <- "/Users/jdp2019/Library/CloudStorage/OneDrive-Emory/Research/MS_raw_data/Chan_studies_MS_raw/PGD_studies/Chan_PGD_9_2025"
 output_dir <- "/Users/jdp2019/Library/CloudStorage/OneDrive-Emory/Research/MS_raw_data/Chan_studies_MS_raw/PGD_studies/Chan_PGD_9_2025/MetaboJanitoR"
 #- Install if needed
-remove.packages("MetaboJanitoR")
-remotes::install_github("jdpreston30/MetaboJanitoR", force = TRUE)
-#- Load
-library(MetaboJanitoR)
-#+ Run with global
-run_metabo_janitor(
-  raw_path,
-  output_dir,
-  msmica_kegg_iterations = c("G"),
-  prompt_timeout = 3,
-  use_ComBat_FT = TRUE
-)
-#+ Notes
+{
+  if ("MetaboJanitoR" %in% rownames(installed.packages())) {
+    remove.packages("MetaboJanitoR")
+  }
+  remotes::install_github("jdpreston30/MetaboJanitoR", force = TRUE, upgrade = "never")
+}
+#- Run
+{
+  library(MetaboJanitoR)
+  raw_path <- "/Users/jdp2019/Library/CloudStorage/OneDrive-Emory/Research/MS_raw_data/Chan_studies_MS_raw/PGD_studies/Chan_PGD_9_2025"
+  output_dir <- "/Users/jdp2019/Library/CloudStorage/OneDrive-Emory/Research/MS_raw_data/Chan_studies_MS_raw/PGD_studies/Chan_PGD_9_2025/MetaboJanitoR"
+  run_metabo_janitor(
+    raw_path,
+    output_dir,
+    msmica_kegg_iterations = c("G"),
+    include_combat = TRUE,
+    prompt_timeout = 3
+  )
+}
+#- Notes
+# ! Updated function on 9/30 to use Combat FT and reran here. New version also got rid of mzm entirely and the Simple/ folder
 # ! H14S2 did not have C18- run for some reason, missing the following samples:
 # ! VT_250824_M623_098, VT_250824_M623_100, VT_250824_M623_102
 # ! So, used the full (available HILIC + full C18) for MSMICA annotation, but had missing HILIC removed from final FTs
--- run.R
+++ run.R
@@ -4,44 +4,14 @@
 {
 source("R/Scripts/00a_environment_setup.R")
 source("R/Scripts/00b_setup.R")
-#+ Imports and Preprocess
 source("R/Scripts/00c_clinical_metadata.R")
 source("R/Scripts/00d_FTs.R")
-#+ Analysis
-#- Main Analysis
 source("R/Scripts/01_clustering.R")
 source("R/Scripts/02_pathway_enrich.R")
+source("R/Scripts/03a_annotated_bars.R")
+source("R/Scripts/03b_targeted_bars.R")
 source("R/Scripts/04_tables.R")
 source("R/Scripts/05_assign_plots.R")
 source("R/Scripts/06_render_figures.R")
+source("R/Scripts/07_data_not_shown.R")
 }
-
-
-source("R/Scripts/03_targeted_bars.R")
-source("R/Scripts/04_tables.R")
-
-#- Visualization
-# source("R/Scripts/05_figures.R")
-#- Data Note Shown
-source("R/Scripts/06_data_not_shown.R")
-#+
-source("R/Scripts/01_clustering.R")
-
-{
-  source("R/Scripts/00b_setup.R")
-  source("R/Scripts/01_clustering.R")
-  source("R/Scripts/05_assign_plots.R")
-  source("R/Scripts/06_render_figures.R")
-}
-
-
-
-# {
-#   source("R/Scripts/00a_environment_setup.R")
-#   source("R/Scripts/00b_setup.R")
-#   source("R/Scripts/00c_clinical_metadata.R")
-#   source("R/Scripts/00d_FTs.R")
-#   source("R/Scripts/01_clustering.R")
-#   source("R/Scripts/05_create_plots.R")
-#   source("R/Scripts/06_render_figures.R")
-# }
